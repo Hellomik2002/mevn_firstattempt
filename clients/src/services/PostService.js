25bcd5351ef3aed7ece1b40d0a4b9e80f477652c
Hellomik2002 <hellomik2002@gmail.com>
2019-01-23 01:13:32 +0600

    good

--- clients/src/services/PostService.js
+++ clients/src/services/PostService.js
@@ -1,4 +1,3 @@
-//import axios from axios;
 import axios from 'axios'
 
 const url = 'http://localhost:8081/api/posts/'
--- clients/src/views/pop.vue
+++ clients/src/views/pop.vue
@@ -1,13 +1,13 @@
 <template>
   
-<div id ="s">
-  <h1>ASLRKALSDKF</h1>
-</div>
+  <div id ="s">
+    <h1>ASLRKALSDKF</h1>
+  </div>
 
 </template>
 
 <script>
-import PostService from "@/services/PostService.js"
+import PostService from '@/services/PostService.js'
 import JQuery from '@/Jquery/jquery.js'
 
 export default {
